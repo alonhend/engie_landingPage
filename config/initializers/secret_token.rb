a3778657010add607f8e5800fc2fdc3ac9c70da6
Alon Hendelman <alon.hendelman@gmail.com>
2014-01-01 13:13:14 +0200

    First Commint
    Empty Rails Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EngieLandingpage::Application.config.secret_key_base = '4549ff2da074270e302e2541f31491d7e03e029374985ab41329f2045201c224117f133420a5a67d4bd13fc4a62fb7b53d3f6edff660e480eb8f46f09c71439a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EngieLandingpage::Application.config.session_store :cookie_store, key: '_engie_landingPage_session'
